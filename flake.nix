eaa0e0ffb4d4d30c7dc1ba024e28e9c41e363fe2
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-22 19:23:02 +0530

    feat: init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "ThatWay dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { ... }@inputs:
+    let
+      forAllSystems =
+        f:
+        inputs.nixpkgs.lib.genAttrs (import inputs.systems) (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.bashInteractive # do not remove
+            ];
+
+            packages = with pkgs; [
+              just # do not remove
+
+              nodejs_22
+              corepack
+            ];
+          };
+        }
+      );
+    };
+}
